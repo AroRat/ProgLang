06638886b036e6a4d51d8ee30b67bb0bbe1da098
AroRat <14172971+arorat@users.noreply.github.com>
2026-09-18 04:39:47 +0800

    part of work (NOT FINAL)

--- lab01/08/3.cpp
+++ lab01/08/3.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 int main() {
     std::vector<int> v(5);
-    for (int x:v)
+    for (auto x:v)
         std::cout << x << ' ';
     return 0;
 }
---
+++ lab01/08/5.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <iostream>
+int main() {
+    std::vector<int> v(5);
+    for (int i=0; i<5; i++)
+       std::cout << v[i] << ' ';
+    std::vector<int> v = {1, 2, 3, 4, 5, 1'000'000'000};
+    return 0;
+}
